6dbbb873bb45b69001cfb6badfa7f8566cee40fd
Karim <severstal.2016@gmail.com>
2023-12-15 17:21:20 +0300

    Relize WebUI

--- wibUiPillow.py
+++ wibUiPillow.py
@@ -21,7 +21,7 @@ def MaskGen(ChanelImg, formula):
     imgMask = ImageMath.eval(formula,a=imgMask,b=imgMackRot).convert(mode='L') # '(a+b)/2'
     return imgMask
 
-def Glich(InputImage,shift,blureRadius):
+def Glich(InputImage,shift,blureRadius,chBox):
     global maskImg
     BlureImage = InputImage.filter(ImageFilter.GaussianBlur(int(blureRadius)))
     InputImage = Image.composite(InputImage,BlureImage,maskImg)
@@ -30,41 +30,37 @@ def Glich(InputImage,shift,blureRadius):
 
     RedOffset = ImageChops.offset(Red,int(shift),0)
 
-    Red = Image.composite(Red,RedOffset,maskImg)
-
+    if chBox:
+        Red = Image.composite(Red,RedOffset,maskImg)
+    else:
+        Red = RedOffset
+    
     ImgOutput = Image.merge('RGB',(Red, Green,Blue))
     #ImgOutput = maskImg
 
     return ImgOutput
 
-def ImgLoad(InputImage,shift,blureRadius):
+def ImgLoad(InputImage,shift,blureRadius,chBox):
     global maskImg
     InputImage = InputImage.convert(mode='RGB')
     maskImg = MaskGen(InputImage,'(a+b)/2')
-    BlureImage = InputImage.filter(ImageFilter.GaussianBlur(int(blureRadius)))
-    InputImage = Image.composite(InputImage,BlureImage,maskImg)
-
-    Red, Green, Blue = InputImage.split()
-
-    RedOffset = ImageChops.offset(Red,int(shift),0)
-
-    Red = Image.composite(Red,RedOffset,maskImg)
-
-    ImgOutput = Image.merge('RGB',(Red, Green,Blue))
-    #ImgOutput = maskImg
+    ImgOutput = Glich(InputImage,shift,blureRadius,chBox)
     return ImgOutput
 
 
 with gr.Blocks(title="УРА ТОВАРИЩИ!") as demo:
-    with gr.Column(scale=10):
+    with gr.Row():
+        with gr.Column(scale=3):
             shift = gr.Slider(0, 10, step=1, value=5,label='Shift')
             blureRadius = gr.Slider(0, 20, step=1, value=5, label='Blure')
+            chBox = gr.Checkbox(value=True,label='Add mask')       
+        with gr.Column(scale=10):
             with gr.Row():
                 input_img = gr.Image(type="pil",width=500)
                 output_img = gr.Image(type="pil",width=500)
             btn = gr.Button("Сделай красиво")
-            btn.click(fn=Glich, inputs=[input_img,shift,blureRadius], outputs=output_img)
-            input_img.upload(fn=ImgLoad,inputs=[input_img,shift,blureRadius], outputs=output_img)
+            btn.click(fn=Glich, inputs=[input_img,shift,blureRadius,chBox], outputs=output_img)
+            input_img.upload(fn=ImgLoad,inputs=[input_img,shift,blureRadius,chBox], outputs=output_img)
 
             
 demo.launch(inbrowser=True, server_port=7860)
